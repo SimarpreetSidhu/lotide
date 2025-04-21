c67f4816bd92a6cbfda691c8d037f8e33b18681a
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-20 23:04:32 -0400

    Simplified the logic as per the AI's feedback

--- head.js
+++ head.js
@@ -7,17 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const head = function(arr) {
-  let head ;
-  if(arr.length != 0){
-    head = arr[0];
-  } else{
-    console.log( `No elemets present in the array`)
-  }
-  
-  return head;
+  return arr[0];
 };
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5]), 5);
-assertEqual(head([]), "Hello");
+assertEqual(head([]), undefined);
