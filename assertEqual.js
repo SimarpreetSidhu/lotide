586e64c64fe9f8a2e436dee2237cbb63dde433bc
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-20 22:43:41 -0400

    Added more scenarios to test assertEqual functionality

--- assertEqual.js
+++ assertEqual.js
@@ -8,3 +8,5 @@ const assertEqual = function(actual, expected) {
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual("Web Development", "Web Development");
+assertEqual(1,3);
