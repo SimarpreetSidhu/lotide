32ddf8256568e74f23445103dfbbc7ade930fe9a
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-20 23:35:59 -0400

    Included more scenarios to test the tail functionality

--- tail.js
+++ tail.js
@@ -16,10 +16,17 @@ assertEqual(result[0] ,"Lighthouse");
 assertEqual(result[1], "Labs");
 assertEqual(result[2], undefined);
 
-const result1 = tail([1,2,3,4,5,6,7,7,7,8,8,8,8,8,8,8,8,9]);
-assertEqual(result1.length , 2);
-assertEqual(result1[0] ,2);
-assertEqual(result1[1], "3");
-assertEqual(result1[2], undefined);
+const result1 = tail([1]);
+assertEqual(result1.length , 0);
+assertEqual(result1[0] , undefined);
+
+const result2 = tail([]);
+assertEqual(result2.length , 0);
+assertEqual(result2[0] , undefined);
+
+const result3 = tail([1,2,3,4,5,6,6,7,87]);
+assertEqual(result3.length , 8);
+assertEqual(result3[0] , undefined);
+
 
 
