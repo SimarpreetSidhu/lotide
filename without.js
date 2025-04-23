5a0f8875e5e4994dffe64559fdd8937a0c646680
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-22 23:24:55 -0400

    Implemented without function which returns a subset of a given array, removing unwanted elements

---
+++ without.js
@@ -0,0 +1,47 @@
+const eqArrays = function(arr1, arr2) {
+  let flag = true;
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        flag = false;
+        break;
+      }
+    }
+  } else {
+    flag = false;
+  }
+  return flag;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1 , arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+ 
+};
+
+const without = function(source, itemsToRemove) {
+  let newArr = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArr.push(source[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(without([1,2,3],[2]));
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+
+
+
+
+
