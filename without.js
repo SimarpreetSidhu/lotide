a88f6f1b173db02099352c9c80d819ce3a9c1b7e
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-22 23:28:47 -0400

    Added more test scenarios

--- without.js
+++ without.js
@@ -40,6 +40,9 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); 
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); 
+
 
 
 
