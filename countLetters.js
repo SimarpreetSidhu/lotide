67fe0ec0413829efebdec84dffa06337cf8fbc3e
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-25 09:51:14 -0400

    To use assertEqual to compare two objects, converted the object entries using JSON.Stringify

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
 const countLetters = function(str) {
   let result = {};
   let noSpacesStr = str.split(" ").join("");
@@ -8,8 +16,25 @@ const countLetters = function(str) {
       result[noSpacesStr[i]] = 1;
     }
   }
-  console.log(result);
   return result;
 };
 
-countLetters(`Simarpreet Kaur`);
+const expectedObject = {
+    l: 1,
+    i: 2,
+    g: 1,
+    h: 4,
+    t: 2,
+    o: 2,
+    u: 2,
+    s: 2,
+    e: 3,
+    n: 1,
+  
+}
+
+const actualObject = countLetters(`lighthouse in the house`);
+const entries1 = JSON.stringify(Object.entries(expectedObject));
+const entries2 = JSON.stringify(Object.entries(actualObject));
+
+assertEqual(entries1,entries2);
