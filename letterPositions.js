1e2bf8ea5ab615776087ed85e3fb1a518f68b056
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-04-25 20:54:17 -0400

    removed console.log , included more test sceanrios

--- letterPositions.js
+++ letterPositions.js
@@ -9,7 +9,6 @@ const letterPositions = function(sentence) {
       }
     }
   }
-  console.log(results)
   return results;
 };
 
@@ -37,5 +36,19 @@ const assertArraysEqual = function(arr1, arr2) {
  
 };
 
+const expectedObject = {
+  l: [0],
+  i: [1, 11],
+  g: [2],
+  h: [3, 5, 15, 18],
+  t: [4, 14],
+  o: [6, 19],
+  u: [7, 20],
+  s: [8, 21],
+  e: [9, 16, 22],
+  n: [12]
+}
+
 assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("lighthouse in the house").e, [9,14,19]);
+assertArraysEqual(letterPositions("lighthouse in the house").e, expectedObject.e);
+assertArraysEqual(letterPositions("lighthouse in the house").h, expectedObject.h);
