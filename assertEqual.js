7bc65521cd6c27755884b8b801b6309340e1de0d
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-05-10 15:18:33 -0400

    Refactored, exported/imported modules , created separate test files

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Web Development", "Web Development");
-assertEqual(1,3);
+module.exports = assertEqual;
